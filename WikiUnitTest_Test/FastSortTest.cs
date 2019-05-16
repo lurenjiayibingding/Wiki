2cb72dd23038fa8804d90a8eff3dc82fe610e7bc
liujgwork <liujg_dr@163.com>
2019-05-16 20:04:46 +0800

    '111'

---
+++ WikiUnitTest_Test/FastSortTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wiki_UnitTest.BLL;
+
+namespace WikiUnitTest_Test
+{
+    [TestClass]
+    public class FastSortTest
+    {
+        [TestMethod]
+        public void FastSortTest1()
+        {
+            int[] input = new int[] { 5, 4, 10, 9, 8, 3, 2, 7, 6, 1 };
+            FastSort fastSort = new FastSort();
+            fastSort.Sort<int>(input, 0, input.Length - 1);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.IsTrue(input[i] == i + 1);
+            }
+        }
+
+        [TestMethod]
+        public void FastSortTest2()
+        {
+            int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            FastSort fastSort = new FastSort();
+            fastSort.Sort<int>(input, 0, input.Length - 1);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.IsTrue(input[i] == i + 1);
+            }
+        }
+
+        [TestMethod]
+        public void FastSortTest3()
+        {
+            int[] input = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            FastSort fastSort = new FastSort();
+            fastSort.Sort<int>(input, 0, input.Length - 1);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.IsTrue(input[i] == i + 1);
+            }
+        }
+    }
+}
---
+++ Wiki_UnitTest/BLL/FastSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiki_UnitTest.BLL
+{
+    public class FastSort
+    {
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="low"></param>
+        /// <param name="height"></param>
+        public void Sort<T>(IList<T> input, int low, int height) where T : IComparable
+        {
+            if (low >= height || height < 0 || low < 0)
+                return;
+
+            if (height - low == 1)
+            {
+                if (input[low].CompareTo(input[height]) >= 0)
+                {
+                    Exchange(input, low, height);
+                }
+                return;
+            }
+
+            int i = low;
+            int j = height;
+            T median = input[low];
+
+            while (i < j)
+            {
+                while (i < j && input[j].CompareTo(median) > 0)
+                {
+                    j--;
+                }
+                while (i < j && input[i].CompareTo(median) <= 0)
+                {
+                    i++;
+                }
+
+                if (i < j)
+                {
+                    Exchange(input, i, j);
+                }
+            }
+            Exchange(input, low, j);
+            Sort(input, low, j - 1);
+            Sort(input, j + 1, height);
+        }
+
+        /// <summary>
+        /// 交换两个元素的位置
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        private void Exchange<T>(IList<T> input, int i, int j)
+        {
+            if (i == j)
+                return;
+            T temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+        }
+    }
+}
