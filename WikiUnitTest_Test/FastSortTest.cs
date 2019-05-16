703fca2af696cbef347bbb53927c479c478b03e3
liujgwork <liujg_dr@163.com>
2019-05-16 20:23:02 +0800

    增加插入排序和插入排序的单元测试

--- WikiUnitTest_Test/FastSortTest.cs
+++ WikiUnitTest_Test/FastSortTest.cs
@@ -13,32 +13,8 @@ namespace WikiUnitTest_Test
         public void FastSortTest1()
         {
             int[] input = new int[] { 5, 4, 10, 9, 8, 3, 2, 7, 6, 1 };
-            FastSort fastSort = new FastSort();
-            fastSort.Sort<int>(input, 0, input.Length - 1);
-            for (int i = 0; i < input.Length; i++)
-            {
-                Assert.IsTrue(input[i] == i + 1);
-            }
-        }
-
-        [TestMethod]
-        public void FastSortTest2()
-        {
-            int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            FastSort fastSort = new FastSort();
-            fastSort.Sort<int>(input, 0, input.Length - 1);
-            for (int i = 0; i < input.Length; i++)
-            {
-                Assert.IsTrue(input[i] == i + 1);
-            }
-        }
-
-        [TestMethod]
-        public void FastSortTest3()
-        {
-            int[] input = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-            FastSort fastSort = new FastSort();
-            fastSort.Sort<int>(input, 0, input.Length - 1);
+            InsertSort fastSort = new InsertSort();
+            fastSort.Sort<int>(input);
             for (int i = 0; i < input.Length; i++)
             {
                 Assert.IsTrue(input[i] == i + 1);
---
+++ Wiki_UnitTest/BLL/InsertSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiki_UnitTest.BLL
+{
+    public class InsertSort
+    {
+        /// <summary>
+        /// 排序方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        public void Sort<T>(IList<T> input) where T : IComparable
+        {
+            if (input.Count <= 1)
+            {
+                return;
+            }
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i].CompareTo(input[i - 1]) < 0)
+                {
+                    var temp = input[i];
+                    int j = i - 1;
+                    for (; j >= 0 && input[j].CompareTo(temp) > 0; j--)
+                    {
+                        input[j + 1] = input[j];
+                    }
+                    input[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
