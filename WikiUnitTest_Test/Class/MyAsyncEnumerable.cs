b47f479b6eaad84ef6bff35d89d3da06e2bda8b7
lurenjia <ljiguang@163.com>
2019-05-22 00:01:16 +0800

    DbSet的测试

---
+++ WikiUnitTest_Test/Class/MyAsyncEnumerable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace WikiUnitTest_Test.Class
+{
+    public class MyAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public MyAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable)
+        { }
+
+        public MyAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetEnumerator()
+        {
+            return new MyAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new MyAsyncQueryProvider<T>(this); }
+        }
+    }
+}
---
+++ WikiUnitTest_Test/Class/MyAsyncEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WikiUnitTest_Test.Class
+{
+    public class MyAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public MyAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public T Current
+        {
+            get
+            {
+                return _inner.Current;
+            }
+        }
+
+        public Task<bool> MoveNext(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+    }
+}
