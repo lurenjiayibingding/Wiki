da3ad6bcc7d322fbbf2f46a74770a3e54367a00e
liujgwork <liujg_dr@163.com>
2019-05-22 17:36:03 +0800

    完成对DbSet的测试

---
+++ WikiUnitTest_Test/Class/DbSetExtend.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WikiUnitTest_Test.Class
+{
+    /// <summary>
+    /// DbSet扩展方法
+    /// </summary>
+    public static class DbSetExtend
+    {
+        /// <summary>
+        /// 设置DbSet
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mockSet"></param>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static Mock<DbSet<T>> SetupArray<T>(this Mock<DbSet<T>> mockSet, params T[] array) where T : class
+        {
+            var queryable = array.AsQueryable();
+            mockSet.As<IAsyncEnumerable<T>>().Setup(m => m.GetEnumerator()).Returns(new MyAsyncEnumerator<T>(queryable.GetEnumerator()));
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(new MyAsyncQueryProvider<T>(queryable.Provider));
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            return mockSet;
+        }
+    }
+}
